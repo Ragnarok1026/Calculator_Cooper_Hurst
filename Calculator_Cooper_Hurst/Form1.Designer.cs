6fc470af0272c68e7b9381dc18a56e297138b8dc
Ragnarok1026 <cooperhurst1026@gmail.com>
2025-09-18 08:33:16 -0500

    gdfhkg4rth
    m bnsgsm

--- Calculator_Cooper_Hurst/Form1.Designer.cs
+++ Calculator_Cooper_Hurst/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(354, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 80);
+            this.label1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1823, 1105);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
